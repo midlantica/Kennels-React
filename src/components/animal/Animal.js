26dcd808ac658b3dc2bf0c913d54be6ca8dd24cb
Andrew Harper <druharper@gmail.com>
2020-04-21 14:46:39 -0500

    few last changes... for now

--- src/components/animal/Animal.js
+++ src/components/animal/Animal.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default ({ animal }) => (
   <section className='animal'>
-    <h5 className='animal__name'>{animal.name}</h5>
+    <h6 className='animal__name'>{animal.name}</h6>
     <p className='animal__breed'>Breed: {animal.breed}</p>
     <p className='animal__breed'>Customer Id: {animal.customerId}</p>
     <p className='animal__breed'>Location Id: {animal.locationId}</p>
--- src/components/location/Location.js
+++ src/components/location/Location.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default (props) => (
   <section className='location'>
-    <h5 className='location__name'>{props.location.name}</h5>
+    <h6 className='location__name'>{props.location.name}</h6>
     <p className='location__address'>{props.location.address}</p>
   </section>
 )
