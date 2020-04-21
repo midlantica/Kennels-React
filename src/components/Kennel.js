b2ec03784ef99051544b86ebed3778fd06f23fb2
Andrew Harper <druharper@gmail.com>
2020-04-21 12:07:52 -0500

    cleaned up the CSS

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,26 +1,31 @@
 import React from 'react'
-import './animal/Animal.css'
+// import './animal/Animal.css'
 import AnimalList from './animal/AnimalList'
 import { AnimalProvider } from './animal/AnimalProvider'
-import './customer/Customer.css'
+// import './customer/Customer.css'
 import CustomerList from './customer/CustomerList'
 import { CustomerProvider } from './customer/CustomerProvider'
-import './employee/Employee.css'
+// import './employee/Employee.css'
 import EmployeeList from './employee/EmployeeList'
 import { EmployeeProvider } from './employee/EmployeeProvider'
-import './location/Location.css'
+// import './Kennel.css'
+// import './location/Location.css'
 import LocationList from './location/LocationList'
 import { LocationProvider } from './location/LocationProvider'
 
 export default () => (
   <>
-    <h2>Nashville Kennels</h2>
-    <small>Loving care when you're not there.</small>
+    <header>
+      <h2>Nashville Kennels</h2>
+      <p>
+        <small>Loving care when you're not there.</small>
+      </p>
+    </header>
     <div>&nbsp;</div>
 
     <address>
-      <div>Visit Us at the Nashville North Location</div>
-      <div>500 Puppy Way</div>
+      <p>Visit Us at the Nashville North Location</p>
+      <p>500 Puppy Way</p>
     </address>
     <hr />
 
